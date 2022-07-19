c48688b7a35875c62798d49aa40a57d0686d5bf0
Niko <43864980+niko-1@users.noreply.github.com>
2022-07-19 11:01:30 +0200

    Update fullpage-menu.js

--- fullpage-menu.js
+++ fullpage-menu.js
@@ -1,4 +1,7 @@
 
+/* This is to make the menu sync with the FullPage navigation in Bricks.
+https://www.facebook.com/groups/brickscommunity/posts/997755410888979?comment_id=997779477553239&reply_comment_id=997801490884371 */
+
 const menuItems = document.querySelectorAll('#myfpmenu > li > a');
 for(let i = 0; i<menuItems.length; i++){
 menuItems[i].addEventListener('click', (event) =>{
